85f57390d5c0ee3fee86784e0c244ab06a5e6e46
Cyrus <sajruz@gmail.com>
2023-02-23 21:05:21 +0000

    Semi-final version

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    videos = ['britney.mp4','jap_speech.mp4','performance.mp4', 'first_fight.mp4', 'second_fight.mp4', 'prozacs_grenades.mp4', 'proZaC_slaps.mp4', 'rail.mp4','prozac_czm.mp4','prozac_zero4.mp4']
+    return render_template('index.html', videos=videos)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
